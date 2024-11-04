1f71fd2fe711489a76c1163950bb06b1f794316c
Seolhun <shun10114@gmail.com>
2024-11-05 06:59:00 +0900

    chore: fix the lint error

--- packages/components/src/stories/descriptor/StorybookContent.Root.tsx
+++ packages/components/src/stories/descriptor/StorybookContent.Root.tsx
@@ -6,12 +6,13 @@ type ElementProps = React.HTMLAttributes<ElementType>;
 
 export interface StorybookContentRootProps {}
 
-export const StorybookContentRoot = React.forwardRef<ElementType, ElementProps & StorybookContentRootProps>(
-  ({ children, className, ...others }, ref) => {
-    return (
-      <div {...others} className={clsx(className, 'flex w-full h-full min-h-screen')} ref={ref}>
-        {children}
-      </div>
-    );
-  },
-);
+export const StorybookContentRoot = React.forwardRef<
+  ElementType,
+  ElementProps & StorybookContentRootProps
+>(({ children, className, ...others }, ref) => {
+  return (
+    <div {...others} className={clsx(className, 'flex w-full h-full min-h-screen')} ref={ref}>
+      {children}
+    </div>
+  );
+});
